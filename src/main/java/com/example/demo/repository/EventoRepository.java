97d9f182dddd7c6ed008bda4c6fea3cd470eb3e5
matiastowers987-collab <matiastowers987@gmail.com>
2026-02-26 23:38:20 -0300

    Delete src directory

--- src/main/java/com/example/demo/repository/EventoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.eventos.Eventos;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventoRepository extends JpaRepository<Eventos,Long> {
-}
